16083f717419cc8050ee7573644eb5008859c918
Boom Cha <ssangbomb@gmail.com>
2023-04-04 03:04:48 +0000

    assert colorful

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  } else if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+console.log("---");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+console.log("---");
+assertEqual(4, 5);
