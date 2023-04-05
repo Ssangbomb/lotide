b513657fcecc3c973c97376e77504ee1a36c664a
Boom Cha <ssangbomb@gmail.com>
2023-04-05 23:56:31 +0000

    get to know object

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetter = function(string) {
+  let countedStr = {};
+  for(let str of string) {
+   if(countedStr[str]) {
+    countedStr[str] += 1;
+   } else {
+    countedStr[str] = 1;
+   }
+  }
+  return countedStr;
+}
+
+console.log(countLetter("lighthouse in the house"));
