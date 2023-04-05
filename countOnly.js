a24264b695c291620f487e62f9b8c5350f719d0f
Boom Cha <ssangbomb@gmail.com>
2023-04-05 23:43:59 +0000

    object is hard to understand man

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let countedList = {};
+  for(let item of allItems) {
+    if(itemsToCount[item]) {
+      if(countedList[item]){
+        console.log(item);
+        countedList[item] += 1;
+      } else {
+        countedList[item] = 1;
+      }
+    }
+  }
+  return countedList;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }))
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
--- middle.js
+++ middle.js
@@ -41,3 +41,4 @@ const middle = function(array) {
 console.log(middle([1, 2, 3, 4, 5]))
 console.log(middle([1, 2, 3, 4, 5, 6]))
 
+x``
