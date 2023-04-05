195386bb1982a8f4ade5917293c129b909194e1e
Boom Cha <ssangbomb@gmail.com>
2023-04-05 01:41:17 +0000

    W01 Day2

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  for(a1 of arr1) {
+    console.log(a1);
+  }
+  for(a2 of arr2) {
+    console.log(a2);
+  }
+  if(a1 === a2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
--- head.js
+++ head.js
@@ -13,7 +13,6 @@ const head = function(array) {
   let firstThing;
   for(let i = 0; i < array.length; i++) {
     firstThing = array[0];
-    console.log(firstThing);
   }
   return firstThing;
 }
