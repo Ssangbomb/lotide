481b83009abd84516a16372e0161ed221b623b86
Boom Cha <ssangbomb@gmail.com>
2023-04-10 09:45:26 +0000

    refactor tail.js middle.js with mocha and chia

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-//test code
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]))
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]))
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-assertEqual(tail([5,6,7]), 7);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+describe("#tail", () => {
+  it("returns 7 for [5,6,7]", () => {
+    assert.strictEqual(tail([5,6,7]), 7);
+  });
+
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs"); 
+  });
+});
+
+
+
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// assertEqual(tail([5,6,7]), 7);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
