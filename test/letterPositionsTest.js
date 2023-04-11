60d11b1e451b79332b5cb332c651bf3df52834b6
Boom Cha <ssangbomb@gmail.com>
2023-04-11 19:43:58 +0000

    minor error fixed

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -4,14 +4,14 @@ const letterPositions = require('../letterPositions');
 
 describe("#letterPositionsTest", () => {
   it("returns [2, 3] for letterPositions['Hello'].e", () => {
-    assert.strictEqual(letterPositions("Hello").l, [2, 3]);
+    assert.deepEqual(letterPositions("Hello").l, [2, 3]);
   });
 
   it("returns [4] for result1['Hello'].o", () => {
-    assert.strictEqual(letterPositions("Hello").o, [4]);
+    assert.deepEqual(letterPositions("Hello").o, [4]);
   });
 
   it("returns [4] for letterPositions['Lighthouse'].t", () => {
-    assert.strictEqual(letterPositions("Lighthouse").t, [4]);
+    assert.deepEqual(letterPositions("Lighthouse").t, [4]);
   });
 });
